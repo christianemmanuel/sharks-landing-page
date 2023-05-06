b375b3bcf68e437cfdd1e20c47a123bc1ea67b54
christianemmanuel <ian34concepcion@gmail.com>
2023-05-06 22:44:40 +0800

    fix countdown event

---
+++ js/countdown.js
@@ -0,0 +1,32 @@
+// Set the date we're counting down to
+var countDownDate = new Date("May 23, 2023 15:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+// Get today's date and time
+var now = new Date().getTime();
+  
+// Find the distance between now and the count down date
+var distance = countDownDate - now;
+  
+// Time calculations for days, hours, minutes and seconds
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+document.getElementById("countdown").innerHTML = `<ul>
+<li><div id="days">${days}</div><span>Days</span></li>
+<li><div id="hours">${hours}</div><span>Hours</span></li>
+<li><div id="minutes">${minutes}</div><span>Minutes</span></li>
+<li><div id="seconds">${seconds}</div><span>Seconds</span></li>
+</ul>`
+
+// If the count down is over, write some msg
+if (distance < 0) {
+  clearInterval(x);
+  document.querySelector('.countdown-event').remove();
+  document.getElementById("countdown").innerHTML = "";
+}
+}, 1000);
